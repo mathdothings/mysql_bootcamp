65ad4a339b737c4a0a599368fec40054bfd91bcf
mathdothings <jhonata.prodrigues@gmail.com>
2023-05-06 21:27:42 -0300

    section 8 exercises solutions

--- sort_by_author_lastname.sql
+++ sort_by_author_lastname.sql
@@ -2,7 +2,7 @@ SELECT
     UPPER(CONCAT_WS(' ',
                     'my favorite author is ',
                     author_fname,
-                    author_lname)) AS yell
+                    CONCAT(author_lname, '!'))) AS yell
 FROM
     books
 ORDER BY author_lname;

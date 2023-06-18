e8e80d3490da23ff634b6ff3c99cd522ca469540
mathdothings <jhonata.prodrigues@gmail.com>
2023-06-18 15:20:41 -0300

    revert wrong commits

--- genre_rating.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-    genre, ROUND(AVG(rating), 2) AS avg_rating
-FROM
-    series
-        INNER JOIN
-    ratings ON series.id = ratings.series_id
-GROUP BY genre;
--- title_rating_vw.sql
+++ title_rating_vw.sql
@@ -1,14 +1,6 @@
 CREATE VIEW title_rating AS
-    SELECT 
-        title, rating
-    FROM
-        series
-            INNER JOIN
-        ratings ON series.id = ratings.series_id;
+	SELECT title, rating FROM series
+	INNER JOIN ratings
+	ON series.id = ratings.series_id;
     
-SELECT 
-    title, CEIL(AVG(rating)) AS average
-FROM
-    title_rating
-GROUP BY title
-ORDER BY average DESC;
+SELECT * FROM title_rating;

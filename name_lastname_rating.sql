114c5fb6e943911a1855bd2d699a26a2104938f3
mathdothings <jhonata.prodrigues@gmail.com>
2023-06-18 15:07:20 -0300

    fixing wrong commit

--- name_lastname_rating.sql
+++
@@ -1,6 +0,0 @@
-SELECT 
-    reviewers.firstname, reviewers.lastname, ratings.rating
-FROM
-    reviewers
-        INNER JOIN
-    ratings ON reviewers.id = ratings.reviewers_id;
--- title_avg_rating.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    series.title, AVG(ratings.rating) AS avg_rating
-FROM
-    series
-        INNER JOIN
-    ratings ON series.id = ratings.series_id
-GROUP BY title
-ORDER BY avg_rating;
